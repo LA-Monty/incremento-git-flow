8f5c6a69d5644a06225021863986c4988995f615
LA-Monty <20230919@itla.edu.do>
2025-11-24 21:25:26 -0400

    Agregar tareas

---
+++ js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById ('task-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+
+    const addTask = (event) => {
+        const taskText = taskInput.value.trim();
+        if(!taskText){
+            return;
+        }
+    // Este script genera las entradas de tareas desde la app y las mismas se generan como un elemento de una columna
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    };
+
+    addTaskBtn.addEventListener('click', addTask);
+    taskInput.addEventListener('Keypress', (e) => {
+        if(e.key === 'Enter') {
+            addTask(e);
+        }
+    });
+
+});
+
